3a79cbbd5d3a2c195a0fd78f2f2bd0adbc3e7b4c
Ayush Shukla <shuklaaa07@gmail.com>
2026-04-19 02:34:43 +0530

    Add gitignore, env config, requirements.txt

--- Backend/config.py
+++ Backend/config.py
@@ -1,9 +1,11 @@
-
-
+import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/doctor_db"
+    SQLALCHEMY_DATABASE_URI        = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "super-secret-key-123"
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)  # lasts 1 day
+    JWT_SECRET_KEY                 = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES       = timedelta(days=1)
